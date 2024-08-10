7b09aa87e5f18474e3f22d3c7db9b43af5581bd9
David Elliott <david@starjump.org>
2024-08-10 19:06:30 +0100

    Initial commit

---
+++ blocks.def.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",    "/usr/local/bin/statusbar/updates",         10,    0},
+	{"",    "/usr/local/bin/statusbar/battery",         10,    0},
+	{"",    "/usr/local/bin/statusbar/volume",         1,     0},
+	{"",    "/usr/local/bin/statusbar/brightness",     1,     0},
+	{"",    "/usr/local/bin/statusbar/clock.sh",       60,     0},
+
+	/*{"",    "date '+%b %d (%a) %I:%M%p'",			   5,	  0},*/
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " ∙ ";
+static unsigned int delimLen = 5;
---
+++ scripts/clock.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+printf "^c#61afef^   ^d^$(date '+%I:%M %p') "
+
